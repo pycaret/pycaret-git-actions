e670c3caddd0f87d9542cc772ac3a4bf0c369e2f
PyCaret <moez@pycaret.org>
2020-07-08 19:29:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import os, ast
 import pandas as pd
 
-pd.set_option("display.max_columns", 101)
-
 dataset_path = "https://raw.githubusercontent.com/" + os.environ["GITHUB_REPOSITORY"] +"/master/dataset.csv"
 data = pd.read_csv(dataset_path)
 data.head()
 
 from pycaret.classification import *
 clf1 = setup(data, target = 'Purchase', session_id=123, silent=True, html=False)
+
+pd.set_option("display.max_columns", 999)
 
 c = compare_models()
 
