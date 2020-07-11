287d88752655ab39d84f31b62031e37264b4a7f0
PyCaret <moez@pycaret.org>
2020-07-10 21:07:00 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ data = pd.read_csv(dataset_path)
 data.head()
 
 from pycaret.classification import *
-clf1 = setup(data, target = 'Purchase', session_id=123, silent=True, html=False)
-
-pd.set_option("display.max_columns", 999)
+clf1 = setup(data, target = 'Purchase', session_id=123, silent=True, html=False, log_experiment=True, experiment_name='juice1')
 
 c = compare_models()
 
 save_model(c, 'model')
+
+logs_juice1 = get_logs(save=True)
