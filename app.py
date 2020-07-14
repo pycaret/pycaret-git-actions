73128cd7a9945ff6b96b63d9157987ffd3cd2b7b
PyCaret <moez@pycaret.org>
2020-07-14 14:48:33 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ dataset_path = "https://raw.githubusercontent.com/" + os.environ["GITHUB_REPOSIT
 data = pd.read_csv(dataset_path)
 data.head()
 
+target = ast.literal_eval(os.environ["INPUT_MYINPUT"])
+
 from pycaret.classification import *
-clf1 = setup(data, target = 'Purchase', session_id=123, silent=True, html=False, log_experiment=True, experiment_name='juice1')
+clf1 = setup(data, target =  target, session_id=123, silent=True, html=False, log_experiment=True, experiment_name='exp1')
 
 c = compare_models()
 
 save_model(c, 'model')
 
-logs_juice1 = get_logs(save=True)
+logs_exp1 = get_logs(save=True)
