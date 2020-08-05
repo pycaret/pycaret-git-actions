1c4a1b7ba54cbb27c18a61b9e3cfe6fcef3e97cb
PyCaret <moez@pycaret.org>
2020-08-04 22:20:12 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ import pandas as pd
 
 dataset = os.environ["INPUT_DATASET"]
 target = os.environ["INPUT_TARGET"]
+usecase = os.environ["INPUT_USECASE"]
 
 dataset_path = "https://raw.githubusercontent.com/" + os.environ["GITHUB_REPOSITORY"] + "/master/" + os.environ["INPUT_DATASET"] + '.csv'
 data = pd.read_csv(dataset_path)
 data.head()
 
-if type(target) is float:
+if usecase == 'regression':
     from pycaret.regression import *
-else:
+elif usecase == 'classification':
     from pycaret.classification import *
 
 exp1 = setup(data, target = target, session_id=123, silent=True, html=False, log_experiment=True, experiment_name='exp_github')
